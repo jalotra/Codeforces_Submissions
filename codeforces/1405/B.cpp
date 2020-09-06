95aebbe5692e345682eba0ecdcacc812fbd77201
Shivam Jalotra <NA>
2020-09-07 00:11:00 +0530

    Add solution for problem `B - Array Cancellation` Link: https://codeforces.com/contest/1405/problem/B Tags: constructive algorithms, implementation, *1000 Ref: https://codeforces.com/contest/1405/submission/92084123

---
+++ codeforces/1405/B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
+
+
+void test_case(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for(int i = 0; i < n; i++)cin >> a[i];
+    
+    long long ans = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] < 0){
+            ans -= min((long long)-a[i], ans);
+        }else{
+            ans += a[i];
+        }
+        //cout << ans << endl;
+    }
+    
+    cout << ans << endl;
+    
+    
+    
+    
+    //cout << abs(mn) << endl;
+    
+} 
+
+
+int main(){
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+    
+    return 0;
+} 
+
+
+ 
