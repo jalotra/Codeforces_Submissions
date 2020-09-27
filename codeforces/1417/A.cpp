0fadfd942ef2316cf6434fb064ac6205f520e5e0
Shivam Jalotra <NA>
2020-09-27 20:47:00 +0530

    Add solution for problem `A - Copy-paste` Link: https://codeforces.com/contest/1417/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1417/submission/93977066

---
+++ codeforces/1417/A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
+
+
+void test_case(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n);
+    int min = INT_MAX;
+    int idx = -1;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] < min){
+            min = a[i];
+            idx = i;
+        }
+    }
+    int ans = 0;
+    for(int i = 0; i  <n; i++){
+        if(i != idx)ans += (k - a[i])/min;
+    }
+    cout << ans << endl;
+    
+} 
+
+
+int main(){
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+    
+    return 0;
+} 
+
+
+ 
