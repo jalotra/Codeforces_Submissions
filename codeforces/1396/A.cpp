7aaff86f35a3c06e535d4b502dbd60afb192a8e4
Shivam Jalotra <NA>
2020-09-27 16:39:00 +0530

    Add solution for problem `A - Multiples of Length` Link: https://codeforces.com/contest/1396/problem/A Tags: constructive algorithms, greedy, number theory, *1600 Ref: https://codeforces.com/contest/1396/submission/93949720

---
+++ codeforces/1396/A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
+
+
+void test_case(){
+    int n;
+    cin >> n;
+    vector<long long>a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    if(n == 1){
+        cout << 1 << " " << 1 << endl;
+        cout << 0 << endl;
+        cout << 1 << " " << 1 << endl;
+        cout << 0 << endl;
+        cout << 1 << " " << 1 << endl;
+        cout << -a[0] << endl;
+    }else{
+        cout << 1 << " " << 1 << endl;
+        cout << -a[0] << endl;
+        cout << 1 << " " << n << endl;
+        for(int i = 0; i < n; i++){
+            if(i==0)cout << 0 << " ";
+            else cout << -n*a[i] << " ";
+        }cout << endl; 
+        cout << 2 << " " << n << endl;
+        for(int i = 1; i < n; i++){
+            cout << (n-1)*a[i] << " ";
+        }cout << endl;
+    }
+    
+} 
+
+
+int main(){
+    
+    int t = 1;
+    //cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+    
+    return 0;
+} 
+
+
+ 
