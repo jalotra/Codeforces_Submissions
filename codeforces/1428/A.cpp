b06b4d8b390f45c3bf2ee51e7bd196cae269ae77
Shivam Jalotra <NA>
2020-10-20 18:42:00 +0530

    Add solution for problem `A - Box is Pull` Link: https://codeforces.com/contest/1428/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1428/submission/96075492

---
+++ codeforces/1428/A.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
+
+
+void test_case(){
+    ll x1,y1,x2,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    
+    int forwards = abs(x2 - x1);
+    int ups = abs(y2 - y1);
+    if(forwards == 0 || ups == 0){
+        cout << ups + forwards << "\n";
+    }else{
+        cout << ups + forwards + 2 << "\n";
+    }
+} 
+
+
+int main(){
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+    
+    return 0;
+} 
+
+
+ 
