86aa3387f3823253c4330fb918cf6365be2df736
Shivam Jalotra <NA>
2020-11-24 20:48:00 +0530

    Add solution for problem `A - Special Permutation` Link: https://codeforces.com/contest/1454/problem/A Tags: constructive algorithms, probabilities, *800 Ref: https://codeforces.com/contest/1454/submission/99450971

---
+++ codeforces/1454/A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
+
+
+void test_case(){
+    int n;
+    cin >> n;
+    vector<int>a;
+    for(int i = n; i >= 1; i--){
+        a.push_back(i);
+    }
+    
+    if(n%2){
+        swap(a[n/2], a[n - 1]);
+    }
+    for(int x : a){
+        cout << x << " ";
+    }cout << endl;
+} 
+
+
+int main(){
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+    
+    return 0;
+} 
+
+
+ 
