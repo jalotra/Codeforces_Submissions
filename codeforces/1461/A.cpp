4f7da3fdd9a5977c54799ef9ec6da51a2ca56f2d
Shivam Jalotra <NA>
2020-12-11 20:14:00 +0530

    Add solution for problem `A - String Generation` Link: https://codeforces.com/contest/1461/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1461/submission/100918846

--- codeforces/1461/A.cpp
+++ codeforces/1461/A.cpp
@@ -1,41 +1,70 @@
-/**
-  Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
-
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
 
 void test_case(){
-
+    
     int n, k;
     cin >> n >> k;
+    
+    int q = n/3;
     string s = "";
-    int q, r;
-    q = n/3;
-    r = n%3;
-    while(q--){
+    for(int i = 0; i < q; i++){
         s += "abc";
     }
-    if(r == 2){
+    int r = n%3;
+    if(r == 1){
+        s += 'a';
+    }else if(r == 2){
         s += "ab";
-    }else if(r == 1){
-        s += "a";
     }
     cout << s << endl;
+    
+    
+    
+    
+    
+} 
 
 
-
-}
-
-int main()
-{
-    // Write your solution here
-
+int main(){
+    
     int t;
     cin >> t;
-    while(t--)test_case();
-
-
+    for(int i = 0; i < t; i++)
+        test_case();
+    
     return 0;
-}
+} 
+
+
+ 
