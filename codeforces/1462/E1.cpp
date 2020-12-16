1f21538dd3b60dffdb7f6d97769550af586623a2
Shivam Jalotra <NA>
2020-12-16 21:15:00 +0530

    Add solution for problem `E1 - Close Tuples (easy version)` Link: https://codeforces.com/contest/1462/problem/E1 Tags: binary search, combinatorics, math, sortings, two pointers, *1500 Ref: https://codeforces.com/contest/1462/submission/101442712

---
+++ codeforces/1462/E1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void test_case(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for(int i = 0; i < n; i++)cin >> a[i];
+
+    sort(a.begin(), a.end());
+    ll ans = 0;
+    for(int i = n - 1; i >= 0; i--){
+        int value = a[i];
+        int idx = lower_bound(a.begin(), a.end(),value - 2) - a.begin();
+        int numbers = i - idx;
+        ans += ((numbers)*1ll*(numbers - 1))/2;
+    }cout << ans << endl;
+
+}
+
+
+int main(){
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+
+    return 0;
+}
