373d17c90df16c503658631e0677c05876f006e2
Shivam Jalotra <NA>
2020-12-20 21:17:00 +0530

    Add solution for problem `B - Fair Numbers` Link: https://codeforces.com/contest/1465/problem/B Tags: brute force, math, number theory, *1000 Ref: https://codeforces.com/contest/1465/submission/101881634

---
+++ codeforces/1465/B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+#define ll long long
+
+bool good(ll n){
+    
+    string s = to_string(n);
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '0')continue;
+        if(n%(s[i] - '0') != 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void test_case(){
+    ll n;
+    cin >> n;
+    //n = RAND_MAX;
+    while(!good(n)){
+        n++;
+    }
+    cout << n << endl;
+} 
+
+
+int main(){
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+        test_case();
+    
+    return 0;
+} 
+
+
+ 
